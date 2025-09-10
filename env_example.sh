a0b6d2bdd30c6811e65ac92d21f41601771723c2
ayushprv <125344421+ayushprv@users.noreply.github.com>
2025-09-10 17:30:45 +0530

    Update env_example.sh

--- env_example.sh
+++ env_example.sh
@@ -1,6 +1,6 @@
 # VirusTotal API Configuration
 # Get your free API key from: https://www.virustotal.com/gui/my-apikey
-VIRUSTOTAL_API_KEY=your_virustotal_api_key_here
+VIRUSTOTAL_API_KEY=cea8e9d9de7821fe4a7076eb7d86119f91fba924a928850587e726553947591f
 
 # Server Configuration
 PORT=3000
